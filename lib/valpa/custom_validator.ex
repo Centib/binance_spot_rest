e6f23001063f785fb7511deb77da7f4f5625e7e9
Zoran Radovanovic <muvacc@gmail.com>
2025-05-23 10:25:15 +0200

    Use Valpa.Error in custom validators

--- lib/valpa/custom_validator.ex
+++ lib/valpa/custom_validator.ex
@@ -1,10 +1,29 @@
 defmodule Valpa.CustomValidator do
   @moduledoc """
   Behaviour for custom validators.
+
+  Implement the `validate/1` callback to return:
+
+  * `:ok` — if the value is valid
+  * `{:error, %Valpa.Error{}}` — if the value is invalid
+
+  Example:
+
+    defmodule MyApp.Validators.PositiveInteger do
+      @behaviour Valpa.CustomValidator
+
+      def validate(val) when is_integer(val) and val > 0, do: :ok
+      def validate(val), do:
+        {:error, Valpa.Error.new(%{
+          validator: :positive_integer,
+          value: val,
+          criteria: "> 0"
+        })}
+    end
   """
 
-  @callback validate(term) :: :ok | {:error, String.t()}
-  @type validate :: (term -> :ok | {:error, String.t()})
+  @callback validate(term) :: :ok | {:error, Valpa.Error.t()}
+  @type validate :: (term -> :ok | {:error, Valpa.Error.t()})
   @type t :: module()
 
   def ensure_behaviour(validator) do
