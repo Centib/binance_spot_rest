1c859262a1c011756d6525bbe67e0270b173f7f8
Zoran Radovanovic <muvacc@gmail.com>
2025-05-27 12:30:47 +0200

    Finish exchange info endpoint

---
+++ lib/binance_spot_rest/endpoints/general/exchange_info/query.ex
@@ -0,0 +1,25 @@
+defmodule BinanceSpotRest.Endpoints.General.ExchangeInfo.Query do
+  @moduledoc """
+  Query - permissions and status available
+  """
+
+  defstruct [:permissions, :showPermissionSets, :symbolStatus]
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> Valpa.maybe_value_or_uniq_list_of_values(
+          :permissions,
+          BinanceSpotRest.Enums.Permission.values()
+        )
+        |> Valpa.maybe_boolean(:showPermissionSets)
+        |> Valpa.maybe_value_of_values(:symbolStatus, BinanceSpotRest.Enums.SymbolStatus.values())
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: BinanceSpotRest.Endpoints.General.ExchangeInfo.Endpoint.metadata(),
+        query: q
+      }
+  end
+end
--- test/binance_spot_rest/endpoints/general/exchange_info_test.exs
+++ test/binance_spot_rest/endpoints/general/exchange_info_test.exs
@@ -3,7 +3,7 @@ defmodule BinanceSpotRest.Endpoints.General.ExchangeInfoTest do
   use ExUnit.Case, async: true
   import Loe
 
-  test "exchange_info symbol request" do
+  test "exchange_info symbol query request" do
     assert {:ok, request} =
              %BinanceSpotRest.Endpoints.General.ExchangeInfo.SymbolQuery{symbol: "BNBBTC"}
              ~>> BinanceSpotRest.Query.validate()
@@ -18,9 +18,29 @@ defmodule BinanceSpotRest.Endpoints.General.ExchangeInfoTest do
            }
   end
 
-  test "exchange_info symbols request" do
+  test "exchange_info symbol and show permission sets query request" do
     assert {:ok, request} =
-             %BinanceSpotRest.Endpoints.General.ExchangeInfo.SymbolsQuery{symbols: ["BTCUSDT", "BNBBTC"]}
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.SymbolQuery{
+               symbol: "BNBBTC",
+               showPermissionSets: false
+             }
+             ~>> BinanceSpotRest.Query.validate()
+             ~>> BinanceSpotRest.Query.prepare()
+             ~>> BinanceSpotRest.Client.create_request()
+
+    assert request == %BinanceSpotRest.Client.Request{
+             method: :get,
+             headers: [],
+             base_url: "https://testnet.binance.vision",
+             url: "/api/v3/exchangeInfo?showPermissionSets=false&symbol=BNBBTC"
+           }
+  end
+
+  test "exchange_info symbols query request" do
+    assert {:ok, request} =
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.SymbolsQuery{
+               symbols: ["BTCUSDT", "BNBBTC"]
+             }
              ~>> BinanceSpotRest.Query.validate()
              ~>> BinanceSpotRest.Query.prepare()
              ~>> BinanceSpotRest.Client.create_request()
@@ -30,6 +50,107 @@ defmodule BinanceSpotRest.Endpoints.General.ExchangeInfoTest do
              headers: [],
              base_url: "https://testnet.binance.vision",
              url: "/api/v3/exchangeInfo?symbols=%5B%22BTCUSDT%22%2C%22BNBBTC%22%5D"
+           }
+  end
+
+  test "exchange_info symbols and show permission sets query sets request" do
+    assert {:ok, request} =
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.SymbolsQuery{
+               symbols: ["BTCUSDT", "BNBBTC"],
+               showPermissionSets: false
+             }
+             ~>> BinanceSpotRest.Query.validate()
+             ~>> BinanceSpotRest.Query.prepare()
+             ~>> BinanceSpotRest.Client.create_request()
+
+    assert request == %BinanceSpotRest.Client.Request{
+             method: :get,
+             headers: [],
+             base_url: "https://testnet.binance.vision",
+             url:
+               "/api/v3/exchangeInfo?showPermissionSets=false&symbols=%5B%22BTCUSDT%22%2C%22BNBBTC%22%5D"
+           }
+  end
+
+  test "exchange_info empty query request" do
+    assert {:ok, request} =
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.Query{}
+             ~>> BinanceSpotRest.Query.validate()
+             ~>> BinanceSpotRest.Query.prepare()
+             ~>> BinanceSpotRest.Client.create_request()
+
+    assert request == %BinanceSpotRest.Client.Request{
+             method: :get,
+             headers: [],
+             base_url: "https://testnet.binance.vision",
+             url: "/api/v3/exchangeInfo"
+           }
+  end
+
+  test "exchange_info permissions query request" do
+    assert {:ok, request} =
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.Query{
+               permissions: [:MARGIN, :LEVERAGED]
+             }
+             ~>> BinanceSpotRest.Query.validate()
+             ~>> BinanceSpotRest.Query.prepare()
+             ~>> BinanceSpotRest.Client.create_request()
+
+    assert request == %BinanceSpotRest.Client.Request{
+             method: :get,
+             headers: [],
+             base_url: "https://testnet.binance.vision",
+             url: "/api/v3/exchangeInfo?permissions=%5B%22MARGIN%22%2C%22LEVERAGED%22%5D"
+           }
+  end
+
+  test "exchange_info symbol status query request" do
+    assert {:ok, request} =
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.Query{symbolStatus: :HALT}
+             ~>> BinanceSpotRest.Query.validate()
+             ~>> BinanceSpotRest.Query.prepare()
+             ~>> BinanceSpotRest.Client.create_request()
+
+    assert request == %BinanceSpotRest.Client.Request{
+             method: :get,
+             headers: [],
+             base_url: "https://testnet.binance.vision",
+             url: "/api/v3/exchangeInfo?symbolStatus=HALT"
+           }
+  end
+
+  test "exchange_info show permission sets query request" do
+    assert {:ok, request} =
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.Query{showPermissionSets: false}
+             ~>> BinanceSpotRest.Query.validate()
+             ~>> BinanceSpotRest.Query.prepare()
+             ~>> BinanceSpotRest.Client.create_request()
+
+    assert request == %BinanceSpotRest.Client.Request{
+             method: :get,
+             headers: [],
+             base_url: "https://testnet.binance.vision",
+             url: "/api/v3/exchangeInfo?showPermissionSets=false"
+           }
+  end
+
+  test "exchange_info permissions, show permission sets and symbol status query request" do
+    assert {:ok, request} =
+             %BinanceSpotRest.Endpoints.General.ExchangeInfo.Query{
+               permissions: [:MARGIN, :LEVERAGED],
+               showPermissionSets: false,
+               symbolStatus: :HALT
+             }
+             ~>> BinanceSpotRest.Query.validate()
+             ~>> BinanceSpotRest.Query.prepare()
+             ~>> BinanceSpotRest.Client.create_request()
+
+    assert request == %BinanceSpotRest.Client.Request{
+             method: :get,
+             headers: [],
+             base_url: "https://testnet.binance.vision",
+             url:
+               "/api/v3/exchangeInfo?permissions=%5B%22MARGIN%22%2C%22LEVERAGED%22%5D&showPermissionSets=false&symbolStatus=HALT"
            }
   end
 end
