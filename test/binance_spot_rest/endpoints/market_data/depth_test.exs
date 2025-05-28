10a115c3dd5a3da380791e703e259617970f5e54
Zoran Radovanovic <muvacc@gmail.com>
2025-05-28 08:45:19 +0200

    Alias query in test

--- test/binance_spot_rest/endpoints/market_data/depth_test.exs
+++ test/binance_spot_rest/endpoints/market_data/depth_test.exs
@@ -1,12 +1,16 @@
 defmodule BinanceSpotRest.Endpoints.MarketData.DepthTest do
   @moduledoc false
+
   use ExUnit.Case, async: true
+
   import Loe
+
+  alias BinanceSpotRest.Endpoints.MarketData.Depth
 
   describe "request" do
     test "symbol" do
       assert {:ok, request} =
-               %BinanceSpotRest.Endpoints.MarketData.Depth.Query{symbol: "BTCUSDT"}
+               %Depth.Query{symbol: "BTCUSDT"}
                ~>> BinanceSpotRest.Query.validate()
                ~>> BinanceSpotRest.Query.prepare()
                ~>> BinanceSpotRest.Client.create_request()
@@ -21,7 +25,7 @@ defmodule BinanceSpotRest.Endpoints.MarketData.DepthTest do
 
     test "symbol and limit" do
       assert {:ok, request} =
-               %BinanceSpotRest.Endpoints.MarketData.Depth.Query{symbol: "BTCUSDT", limit: 150}
+               %Depth.Query{symbol: "BTCUSDT", limit: 150}
                ~>> BinanceSpotRest.Query.validate()
                ~>> BinanceSpotRest.Query.prepare()
                ~>> BinanceSpotRest.Client.create_request()
@@ -38,23 +42,23 @@ defmodule BinanceSpotRest.Endpoints.MarketData.DepthTest do
   describe "validation" do
     test "invalid without symbol" do
       assert {:error, _} =
-               %BinanceSpotRest.Endpoints.MarketData.Depth.Query{symbol: nil}
+               %Depth.Query{symbol: nil}
                ~>> BinanceSpotRest.Query.validate()
 
       assert {:error, _} =
-               %BinanceSpotRest.Endpoints.MarketData.Depth.Query{}
+               %Depth.Query{}
                ~>> BinanceSpotRest.Query.validate()
     end
 
     test "invalid with non-string symbol" do
       assert {:error, _} =
-               %BinanceSpotRest.Endpoints.MarketData.Depth.Query{symbol: :BTCUSDT}
+               %Depth.Query{symbol: :BTCUSDT}
                ~>> BinanceSpotRest.Query.validate()
     end
 
     test "invalid not in range limit" do
       assert {:error, _} =
-               %BinanceSpotRest.Endpoints.MarketData.Depth.Query{
+               %Depth.Query{
                  symbol: "BTCUSDT",
                  limit: 5001
                }
@@ -63,7 +67,7 @@ defmodule BinanceSpotRest.Endpoints.MarketData.DepthTest do
 
     test "valid in range limit" do
       assert {:ok, _} =
-               %BinanceSpotRest.Endpoints.MarketData.Depth.Query{
+               %Depth.Query{
                  symbol: "BTCUSDT",
                  limit: 5000
                }
