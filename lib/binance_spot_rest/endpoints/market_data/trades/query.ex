44615246d45deb3d106b47979e451332daee5f16
Zoran Radovanovic <muvacc@gmail.com>
2025-05-31 11:11:07 +0200

    Add trades endpoint

---
+++ lib/binance_spot_rest/endpoints/market_data/trades/query.ex
@@ -0,0 +1,60 @@
+defmodule BinanceSpotRest.Endpoints.MarketData.Trades.Query do
+  @moduledoc """
+  ### Recent trades list
+
+  ```
+  GET /api/v3/trades
+  ```
+
+  Get recent trades.
+
+  **Weight:**
+  25
+
+  **Parameters:**
+
+  | Name   | Type   | Mandatory | Description            |
+  | ------ | ------ | --------- | ---------------------- |
+  | symbol | STRING | YES       |                        |
+  | limit  | INT    | NO        | Default 500; max 1000. |
+
+  **Data Source:**
+  Memory
+
+  **Response:**
+
+  ```
+  [
+    {
+      "id": 28457,
+      "price": "4.00000100",
+      "qty": "12.00000000",
+      "quoteQty": "48.000012",
+      "time": 1499865549590,
+      "isBuyerMaker": true,
+      "isBestMatch": true
+    }
+  ]
+  ```
+  """
+
+  defstruct [:symbol, :limit]
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> Valpa.string(:symbol)
+        |> Valpa.maybe_integer_in_range(:limit, %{min: 1, max: 1000})
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: %BinanceSpotRest.Query.EndpointMetadata{
+          endpoint: "/api/v3/trades",
+          method: BinanceSpotRest.Enums.Method._get(),
+          security_type: BinanceSpotRest.Enums.SecurityType._NONE()
+        },
+        query: q
+      }
+  end
+end
---
+++ test/binance_spot_rest/endpoints/market_data/trades_test.exs
@@ -0,0 +1,77 @@
+defmodule BinanceSpotRest.Endpoints.MarketData.TradesTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  import Loe
+
+  alias BinanceSpotRest.Endpoints.MarketData.Trades
+
+  describe "request" do
+    test "symbol" do
+      assert {:ok, request} =
+               %Trades.Query{symbol: "BTCUSDT"}
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request()
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: :get,
+               headers: [],
+               base_url: "https://testnet.binance.vision",
+               url: "/api/v3/trades?symbol=BTCUSDT"
+             }
+    end
+
+    test "symbol and limit" do
+      assert {:ok, request} =
+               %Trades.Query{symbol: "BTCUSDT", limit: 150}
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request()
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: :get,
+               headers: [],
+               base_url: "https://testnet.binance.vision",
+               url: "/api/v3/trades?limit=150&symbol=BTCUSDT"
+             }
+    end
+  end
+
+  describe "validation" do
+    test "invalid without symbol" do
+      assert {:error, _} =
+               %Trades.Query{symbol: nil}
+               ~>> BinanceSpotRest.Query.validate()
+
+      assert {:error, _} =
+               %Trades.Query{}
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "invalid with non-string symbol" do
+      assert {:error, _} =
+               %Trades.Query{symbol: :BTCUSDT}
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "invalid not in range limit" do
+      assert {:error, _} =
+               %Trades.Query{
+                 symbol: "BTCUSDT",
+                 limit: 1001
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "valid in range limit" do
+      assert {:ok, _} =
+               %Trades.Query{
+                 symbol: "BTCUSDT",
+                 limit: 1000
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+end
