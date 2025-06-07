81a849553bcc452b8b2f75fbaedf4c5e7465a0d4
Zoran Radovanovic <muvacc@gmail.com>
2025-06-07 06:13:38 +0200

    Add order cancel replace limit query

---
+++ lib/binance_spot_rest/endpoints/trading/order_cancel_replace_post/limit_query.ex
@@ -0,0 +1,22 @@
+defmodule BinanceSpotRest.Endpoints.Trading.OrderCancelReplacePost.LimitQuery do
+  @moduledoc """
+  Order cancel replace: limit query
+  """
+
+  defstruct BinanceSpotRest.Endpoints.Trading.OrderPost.Shared.Limit.fields() ++
+              BinanceSpotRest.Endpoints.Trading.OrderCancelReplacePost.Common.fields()
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> BinanceSpotRest.Endpoints.Trading.OrderPost.Shared.Limit.validation()
+        |> BinanceSpotRest.Endpoints.Trading.OrderCancelReplacePost.Common.validation()
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: BinanceSpotRest.Endpoints.Trading.OrderCancelReplacePost.Endpoint.metadata(),
+        query: q
+      }
+  end
+end
