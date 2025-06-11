957ed89512ea17360e3e9fc04efd0aecff46b220
Zoran Radovanovic <muvacc@gmail.com>
2025-06-11 12:14:32 +0200

    Add order list oco above stop loss limit below take profit query

---
+++ lib/binance_spot_rest/endpoints/trading/order_list_oco_post/above_stop_loss_limit_below_take_profit_query.ex
@@ -0,0 +1,25 @@
+defmodule BinanceSpotRest.Endpoints.Trading.OrderListOcoPost.AboveStopLossLimitBelowTakeProfitQuery do
+  @moduledoc """
+  Order list oco: above stop loss limit below take profit query
+  """
+
+  alias BinanceSpotRest.Endpoints.Trading.OrderPost.Shared.StopLossLimit, as: Above
+  alias BinanceSpotRest.Endpoints.Trading.OrderPost.Shared.TakeProfit, as: Below
+
+  defstruct BinanceSpotRest.Endpoints.Trading.OrderListOcoPost.UseShared.fields(Above, Below) ++
+              BinanceSpotRest.Endpoints.Trading.OrderListOcoPost.Common.fields()
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> BinanceSpotRest.Endpoints.Trading.OrderListOcoPost.UseShared.validation(Above, Below)
+        |> BinanceSpotRest.Endpoints.Trading.OrderListOcoPost.Common.validation()
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: BinanceSpotRest.Endpoints.Trading.OrderListOcoPost.Endpoint.metadata(),
+        query: q
+      }
+  end
+end
