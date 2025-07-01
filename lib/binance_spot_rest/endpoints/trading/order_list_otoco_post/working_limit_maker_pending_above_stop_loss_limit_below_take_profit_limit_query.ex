ea616a08e4033150983b61516840d84091c4777f
Zoran Radovanovic <muvacc@gmail.com>
2025-07-01 10:43:00 +0200

    Add order list otoco working limit maker pending above stop loss limit below take profit limit query

---
+++ lib/binance_spot_rest/endpoints/trading/order_list_otoco_post/working_limit_maker_pending_above_stop_loss_limit_below_take_profit_limit_query.ex
@@ -0,0 +1,36 @@
+defmodule BinanceSpotRest.Endpoints.Trading.OrderListOtocoPost.WorkingLimitMakerPendingAboveStopLossLimitBelowTakeProfitLimitQuery do
+  @moduledoc """
+  Order list otoco: working limit maker pending above stop loss limit below take profit limit query
+  """
+
+  alias BinanceSpotRest.Endpoints.Trading.OrderPost.Shared.LimitMaker, as: Working
+  alias BinanceSpotRest.Endpoints.Trading.OrderPost.Shared.StopLossLimit, as: PendingAbove
+  alias BinanceSpotRest.Endpoints.Trading.OrderPost.Shared.TakeProfitLimit, as: PendingBelow
+
+  defstruct BinanceSpotRest.Endpoints.Trading.OrderListOtocoPost.UseShared.fields(
+              Working,
+              PendingAbove,
+              PendingBelow,
+              pendingSide: BinanceSpotRest.Enums.Side._BUY()
+            ) ++ BinanceSpotRest.Endpoints.Trading.OrderListOtocoPost.Common.fields()
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> BinanceSpotRest.Endpoints.Trading.OrderListOtocoPost.UseShared.validation(
+          Working,
+          PendingAbove,
+          PendingBelow
+        )
+        |> Valpa.value_of_values(:pendingSide, [BinanceSpotRest.Enums.Side._BUY()])
+        |> BinanceSpotRest.Endpoints.Trading.OrderListOtocoPost.Common.validation()
+        |> Valpa.map_compare_decimal_keys({:>, :pendingAboveStopPrice, :pendingBelowStopPrice})
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: BinanceSpotRest.Endpoints.Trading.OrderListOtocoPost.Endpoint.metadata(),
+        query: q
+      }
+  end
+end
