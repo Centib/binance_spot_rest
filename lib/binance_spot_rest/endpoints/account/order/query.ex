a088dd0d6ace6187c16b73656200c8c8fb149df8
Zoran Radovanovic <muvacc@gmail.com>
2025-07-04 12:33:24 +0200

    Add order get query

---
+++ lib/binance_spot_rest/endpoints/account/order/query.ex
@@ -0,0 +1,85 @@
+defmodule BinanceSpotRest.Endpoints.Account.Order.Query do
+  @moduledoc """
+  ### Query order (USER_DATA)
+
+  ```
+  GET /api/v3/order
+  ```
+
+  Check an order's status.
+
+  **Weight:**
+  4
+
+  **Parameters:**
+
+  | Name              | Type   | Mandatory | Description                              |
+  | ----------------- | ------ | --------- | ---------------------------------------- |
+  | symbol            | STRING | YES       |                                          |
+  | orderId           | LONG   | NO        |                                          |
+  | origClientOrderId | STRING | NO        |                                          |
+  | recvWindow        | LONG   | NO        | The value cannot be greater than `60000` |
+  | timestamp         | LONG   | YES       |                                          |
+
+  **Notes:**
+
+  - Either `orderId` or `origClientOrderId` must be sent.
+  - If both `orderId` and `origClientOrderId` are provided, the `orderId` is searched first, then the `origClientOrderId` from that result is checked against that order. If both conditions are not met the request will be rejected.
+  - For some historical orders `cummulativeQuoteQty` will be < 0, meaning the data is not available at this time.
+
+  **Data Source:**
+  Memory => Database
+
+  **Response:**
+
+  ```javascript
+  {
+    "symbol": "LTCBTC",
+    "orderId": 1,
+    "orderListId": -1,                 // This field will always have a value of -1 if not an order list.
+    "clientOrderId": "myOrder1",
+    "price": "0.1",
+    "origQty": "1.0",
+    "executedQty": "0.0",
+    "cummulativeQuoteQty": "0.0",
+    "status": "NEW",
+    "timeInForce": "GTC",
+    "type": "LIMIT",
+    "side": "BUY",
+    "stopPrice": "0.0",
+    "icebergQty": "0.0",
+    "time": 1499827319559,
+    "updateTime": 1499827319559,
+    "isWorking": true,
+    "workingTime":1499827319559,
+    "origQuoteOrderQty": "0.000000",
+    "selfTradePreventionMode": "NONE"
+  }
+  ```
+
+  **Note:** The payload above does not show all fields that can appear. Please refer to [Conditional fields in Order Responses](#conditional-fields-in-order-responses).
+  """
+
+  defstruct [:symbol, :orderId, :origClientOrderId, :recvWindow]
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> Valpa.string(:symbol)
+        |> Valpa.maybe_integer(:orderId)
+        |> Valpa.maybe_string(:origClientOrderId)
+        |> Valpa.map_inclusive_keys([:orderId, :origClientOrderId])
+        |> Valpa.maybe_integer_in_range(:recvWindow, %{min: 0, max: 60_000})
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: %BinanceSpotRest.Query.EndpointMetadata{
+          endpoint: "/api/v3/order",
+          method: BinanceSpotRest.Enums.Method._get(),
+          security_type: BinanceSpotRest.Enums.SecurityType._USER_DATA()
+        },
+        query: q
+      }
+  end
+end
---
+++ test/binance_spot_rest/endpoints/account/order_test.exs
@@ -0,0 +1,124 @@
+defmodule BinanceSpotRest.Endpoints.Account.OrderTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  import Loe
+
+  alias BinanceSpotRest.Endpoints.Account.Order
+
+  def full_valid_query do
+    %Order.Query{
+      symbol: "LTCBTC",
+      orderId: 1234,
+      origClientOrderId: "origClientOrderId",
+      recvWindow: 3000
+    }
+  end
+
+  describe "request" do
+    test "all params" do
+      assert {:ok, request} =
+               full_valid_query()
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request(
+                 base_url: "https://fake.binance.url",
+                 headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+                 timestamp_fn: fn -> 1_740_587_673_449 end,
+                 signature_fn: fn _, _ -> "fake_signature" end
+               )
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: BinanceSpotRest.Enums.Method._get(),
+               headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+               base_url: "https://fake.binance.url",
+               url:
+                 "/api/v3/order?" <>
+                   "orderId=1234&" <>
+                   "origClientOrderId=origClientOrderId&" <>
+                   "recvWindow=3000&" <>
+                   "symbol=LTCBTC&" <>
+                   "timestamp=1740587673449&" <>
+                   "signature=fake_signature"
+             }
+    end
+  end
+
+  describe "validation (required):" do
+    @required [:symbol]
+
+    for field <- @required do
+      test "invalid without #{field}" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query()
+                 ~>> Map.from_struct()
+                 ~>> Map.delete(unquote(field))
+                 ~>> then(&struct(Order.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+  end
+
+  describe "validation (optional):" do
+    @optional [:recvWindow]
+
+    test "valid without optional fields" do
+      assert {:ok, %Order.Query{}} =
+               full_valid_query()
+               ~>> Map.from_struct()
+               ~>> Map.drop(@optional)
+               ~>> then(&struct(Order.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+
+  describe "validation (type):" do
+    @bad_types [
+      symbol: :LTCBTC,
+      orderId: "1234",
+      origClientOrderId: 5678,
+      recvWindow: "3000"
+    ]
+
+    for {field, bad_value} <- @bad_types do
+      test "invalid #{field} type" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query()
+                 ~>> Map.from_struct()
+                 ~>> Map.put(unquote(field), unquote(Macro.escape(bad_value)))
+                 ~>> then(&struct(Order.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+  end
+
+  describe "validation (inclusive):" do
+    @inclusive [:orderId, :origClientOrderId]
+
+    test "valid with one of #{inspect(@inclusive)}" do
+      assert {:ok, %Order.Query{}} =
+               full_valid_query()
+               ~>> Map.from_struct()
+               ~>> Map.delete(:orderId)
+               ~>> then(&struct(Order.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+
+      assert {:ok, %Order.Query{}} =
+               full_valid_query()
+               ~>> Map.from_struct()
+               ~>> Map.delete(:origClientOrderId)
+               ~>> then(&struct(Order.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "invalid if both #{inspect(@inclusive)} missing" do
+      assert {:error, %{validator: :map_inclusive_keys, criteria: @inclusive}} =
+               full_valid_query()
+               ~>> Map.from_struct()
+               ~>> Map.drop(@inclusive)
+               ~>> then(&struct(Order.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+end
