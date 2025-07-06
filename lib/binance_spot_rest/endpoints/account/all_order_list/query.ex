af4e78acf49b2693bd84825cfa4d6ea1509b4692
Zoran Radovanovic <muvacc@gmail.com>
2025-07-06 08:14:09 +0200

    Add all order list get query

---
+++ lib/binance_spot_rest/endpoints/account/all_order_list/query.ex
@@ -0,0 +1,104 @@
+defmodule BinanceSpotRest.Endpoints.Account.AllOrderList.Query do
+  @moduledoc """
+  ### Query all Order lists (USER_DATA)
+
+  ```
+  GET /api/v3/allOrderList
+  ```
+
+  Retrieves all order lists based on provided optional parameters.
+
+  Note that the time between `startTime` and `endTime` can't be longer than 24 hours.
+
+  **Weight:**
+  20
+
+  **Parameters:**
+
+  | Name       | Type | Mandatory | Description                                                   |
+  | ---------- | ---- | --------- | ------------------------------------------------------------- |
+  | fromId     | LONG | NO        | If supplied, neither `startTime` or `endTime` can be provided |
+  | startTime  | LONG | NO        |                                                               |
+  | endTime    | LONG | NO        |                                                               |
+  | limit      | INT  | NO        | Default: 500; Maximum: 1000                                   |
+  | recvWindow | LONG | NO        | The value cannot be greater than `60000`                      |
+  | timestamp  | LONG | YES       |                                                               |
+
+  **Data Source:**
+  Database
+
+  **Response:**
+
+  ```javascript
+  [
+    {
+      "orderListId": 29,
+      "contingencyType": "OCO",
+      "listStatusType": "EXEC_STARTED",
+      "listOrderStatus": "EXECUTING",
+      "listClientOrderId": "amEEAXryFzFwYF1FeRpUoZ",
+      "transactionTime": 1565245913483,
+      "symbol": "LTCBTC",
+      "orders": [
+        {
+          "symbol": "LTCBTC",
+          "orderId": 4,
+          "clientOrderId": "oD7aesZqjEGlZrbtRpy5zB"
+        },
+        {
+          "symbol": "LTCBTC",
+          "orderId": 5,
+          "clientOrderId": "Jr1h6xirOxgeJOUuYQS7V3"
+        }
+      ]
+    },
+    {
+      "orderListId": 28,
+      "contingencyType": "OCO",
+      "listStatusType": "EXEC_STARTED",
+      "listOrderStatus": "EXECUTING",
+      "listClientOrderId": "hG7hFNxJV6cZy3Ze4AUT4d",
+      "transactionTime": 1565245913407,
+      "symbol": "LTCBTC",
+      "orders": [
+        {
+          "symbol": "LTCBTC",
+          "orderId": 2,
+          "clientOrderId": "j6lFOfbmFMRjTYA7rRJ0LP"
+        },
+        {
+          "symbol": "LTCBTC",
+          "orderId": 3,
+          "clientOrderId": "z0KCjOdditiLS5ekAFtK81"
+        }
+      ]
+    }
+  ]
+  ```
+  """
+
+  defstruct [:fromId, :startTime, :endTime, :limit, :recvWindow]
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> Valpa.maybe_integer(:fromId)
+        |> Valpa.maybe_integer(:startTime)
+        |> Valpa.maybe_integer(:endTime)
+        |> Valpa.map_exclusive_optional_keys([:fromId, :startTime])
+        |> Valpa.map_exclusive_optional_keys([:fromId, :endTime])
+        |> Valpa.maybe_integer_in_range(:limit, %{min: 0, max: 1000})
+        |> Valpa.maybe_integer_in_range(:recvWindow, %{min: 0, max: 60_000})
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: %BinanceSpotRest.Query.EndpointMetadata{
+          endpoint: "/api/v3/allOrderList",
+          method: BinanceSpotRest.Enums.Method._get(),
+          security_type: BinanceSpotRest.Enums.SecurityType._USER_DATA()
+        },
+        query: q
+      }
+  end
+end
---
+++ test/binance_spot_rest/endpoints/account/all_order_list_test.exs
@@ -0,0 +1,166 @@
+defmodule BinanceSpotRest.Endpoints.Account.AllOrderListTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  import Loe
+
+  alias BinanceSpotRest.Endpoints.Account.AllOrderList
+
+  def full_valid_query_with_order_id do
+    %AllOrderList.Query{
+      fromId: 1234,
+      limit: 600,
+      recvWindow: 3000
+    }
+  end
+
+  def full_valid_query_with_time do
+    %AllOrderList.Query{
+      startTime: 1_751_624_374_960,
+      endTime: 1_751_624_974_980,
+      limit: 600,
+      recvWindow: 3000
+    }
+  end
+
+  describe "request" do
+    test "all params" do
+      assert {:ok, request} =
+               full_valid_query_with_order_id()
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request(
+                 base_url: "https://fake.binance.url",
+                 headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+                 timestamp_fn: fn -> 1_740_587_673_449 end,
+                 signature_fn: fn _, _ -> "fake_signature" end
+               )
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: BinanceSpotRest.Enums.Method._get(),
+               headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+               base_url: "https://fake.binance.url",
+               url:
+                 "/api/v3/allOrderList?" <>
+                   "fromId=1234&" <>
+                   "limit=600&" <>
+                   "recvWindow=3000&" <>
+                   "timestamp=1740587673449&" <>
+                   "signature=fake_signature"
+             }
+
+      assert {:ok, request} =
+               full_valid_query_with_time()
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request(
+                 base_url: "https://fake.binance.url",
+                 headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+                 timestamp_fn: fn -> 1_740_587_673_449 end,
+                 signature_fn: fn _, _ -> "fake_signature" end
+               )
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: BinanceSpotRest.Enums.Method._get(),
+               headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+               base_url: "https://fake.binance.url",
+               url:
+                 "/api/v3/allOrderList?" <>
+                   "endTime=1751624974980&" <>
+                   "limit=600&" <>
+                   "recvWindow=3000&" <>
+                   "startTime=1751624374960&" <>
+                   "timestamp=1740587673449&" <>
+                   "signature=fake_signature"
+             }
+    end
+  end
+
+  describe "validation (optional):" do
+    @optional_with_order_id [:fromId, :limit, :recvWindow]
+
+    @optional_with_time [:startTime, :endTime, :limit, :recvWindow]
+
+    test "valid without optional fields" do
+      assert {:ok, %AllOrderList.Query{}} =
+               full_valid_query_with_order_id()
+               ~>> Map.from_struct()
+               ~>> Map.drop(@optional_with_order_id)
+               ~>> then(&struct(AllOrderList.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+
+      assert {:ok, %AllOrderList.Query{}} =
+               full_valid_query_with_time()
+               ~>> Map.from_struct()
+               ~>> Map.drop(@optional_with_time)
+               ~>> then(&struct(AllOrderList.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+
+  describe "validation (type):" do
+    @bad_types [
+      fromId: "1234",
+      limit: "600",
+      recvWindow: "3000"
+    ]
+
+    @bad_types_with_time [
+      startTime: "1_751_624_374_960",
+      endTime: "1_751_624_974_980"
+    ]
+
+    for {field, bad_value} <- @bad_types do
+      test "invalid #{field} type" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query_with_order_id()
+                 ~>> Map.from_struct()
+                 ~>> Map.put(unquote(field), unquote(Macro.escape(bad_value)))
+                 ~>> then(&struct(AllOrderList.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+
+    for {field, bad_value} <- @bad_types_with_time do
+      test "invalid #{field} type" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query_with_time()
+                 ~>> Map.from_struct()
+                 ~>> Map.put(unquote(field), unquote(Macro.escape(bad_value)))
+                 ~>> then(&struct(AllOrderList.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+  end
+
+  describe "validation (exclusive optional keys):" do
+    test "invalid if fromId is with startTime and endTime" do
+      assert {:error, %{validator: :map_exclusive_optional_keys}} =
+               %AllOrderList.Query{
+                 fromId: 1234,
+                 startTime: 1_751_624_374_960,
+                 endTime: 1_751_624_974_980
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "invalid if fromId is with startTime" do
+      assert {:error, %{validator: :map_exclusive_optional_keys}} =
+               %AllOrderList.Query{
+                 fromId: 1234,
+                 startTime: 1_751_624_374_960
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "invalid if fromId is with endTime" do
+      assert {:error, %{validator: :map_exclusive_optional_keys}} =
+               %AllOrderList.Query{
+                 fromId: 1234,
+                 endTime: 1_751_624_974_980
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+end
