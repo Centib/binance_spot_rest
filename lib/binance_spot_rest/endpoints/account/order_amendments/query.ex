a30e28b08eec505df5d9b4b20f39e346f15984e7
Zoran Radovanovic <muvacc@gmail.com>
2025-07-08 16:20:50 +0200

    Add order amendments get query

---
+++ lib/binance_spot_rest/endpoints/account/order_amendments/query.ex
@@ -0,0 +1,79 @@
+defmodule BinanceSpotRest.Endpoints.Account.OrderAmendments.Query do
+  @moduledoc """
+  ### Query Order Amendments (USER_DATA)
+
+  ```
+  GET /api/v3/order/amendments
+  ```
+
+  Queries all amendments of a single order.
+
+  **Weight**:
+  4
+
+  **Parameters:**
+
+  | Name            | Type   | Mandatory | Description                               |
+  | :-------------- | :----- | :-------- | :---------------------------------------- |
+  | symbol          | STRING | YES       |                                           |
+  | orderId         | LONG   | YES       |                                           |
+  | fromExecutionId | LONG   | NO        |                                           |
+  | limit           | LONG   | NO        | Default:500; Maximum: 1000                |
+  | recvWindow      | LONG   | NO        | The value cannot be greater than `60000`. |
+  | timestamp       | LONG   | YES       |                                           |
+
+  **Data Source:**
+
+  Database
+
+  **Response:**
+
+  ```json
+  [
+    {
+        "symbol": "BTCUSDT",
+        "orderId": 9,
+        "executionId": 22,
+        "origClientOrderId": "W0fJ9fiLKHOJutovPK3oJp",
+        "newClientOrderId": "UQ1Np3bmQ71jJzsSDW9Vpi",
+        "origQty": "5.00000000",
+        "newQty": "4.00000000",
+        "time": 1741669661670
+    },
+    {
+        "symbol": "BTCUDST",
+        "orderId": 9,
+        "executionId": 25,
+        "origClientOrderId": "UQ1Np3bmQ71jJzsSDW9Vpi",
+        "newClientOrderId": "5uS0r35ohuQyDlCzZuYXq2",
+        "origQty": "4.00000000",
+        "newQty": "3.00000000",
+        "time": 1741672924895
+    }
+  ]
+  ```
+  """
+
+  defstruct [:symbol, :orderId, :fromExecutionId, :limit, :recvWindow]
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> Valpa.string(:symbol)
+        |> Valpa.integer(:orderId)
+        |> Valpa.maybe_integer(:fromExecutionId)
+        |> Valpa.maybe_integer_in_range(:limit, %{min: 0, max: 1000})
+        |> Valpa.maybe_integer_in_range(:recvWindow, %{min: 0, max: 60_000})
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: %BinanceSpotRest.Query.EndpointMetadata{
+          endpoint: "/api/v3/order/amendments",
+          method: BinanceSpotRest.Enums.Method._get(),
+          security_type: BinanceSpotRest.Enums.SecurityType._USER_DATA()
+        },
+        query: q
+      }
+  end
+end
---
+++ test/binance_spot_rest/endpoints/account/order_amendments_test.exs
@@ -0,0 +1,98 @@
+defmodule BinanceSpotRest.Endpoints.Account.OrderAmendmentsTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  import Loe
+
+  alias BinanceSpotRest.Endpoints.Account.OrderAmendments
+
+  def full_valid_query do
+    %OrderAmendments.Query{
+      symbol: "LTCBTC",
+      orderId: 1234,
+      fromExecutionId: 5678,
+      limit: 600,
+      recvWindow: 3000
+    }
+  end
+
+  describe "request" do
+    test "all params" do
+      assert {:ok, request} =
+               full_valid_query()
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request(
+                 base_url: "https://fake.binance.url",
+                 headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+                 timestamp_fn: fn -> 1_740_587_673_449 end,
+                 signature_fn: fn _, _ -> "fake_signature" end
+               )
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: BinanceSpotRest.Enums.Method._get(),
+               headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+               base_url: "https://fake.binance.url",
+               url:
+                 "/api/v3/order/amendments?" <>
+                   "fromExecutionId=5678&" <>
+                   "limit=600&" <>
+                   "orderId=1234&" <>
+                   "recvWindow=3000&" <>
+                   "symbol=LTCBTC&" <>
+                   "timestamp=1740587673449&" <>
+                   "signature=fake_signature"
+             }
+    end
+  end
+
+  describe "validation (required):" do
+    @required [:symbol, :orderId]
+
+    for field <- @required do
+      test "invalid without #{field}" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query()
+                 ~>> Map.from_struct()
+                 ~>> Map.delete(unquote(field))
+                 ~>> then(&struct(OrderAmendments.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+  end
+
+  describe "validation (optional):" do
+    @optional [:fromExecutionId, :limit, :recvWindow]
+
+    test "valid without optional fields" do
+      assert {:ok, %OrderAmendments.Query{}} =
+               full_valid_query()
+               ~>> Map.from_struct()
+               ~>> Map.drop(@optional)
+               ~>> then(&struct(OrderAmendments.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+
+  describe "validation (type):" do
+    @bad_types [
+      symbol: :LTCBTC,
+      orderId: "1234",
+      fromExecutionId: "5678",
+      limit: "600",
+      recvWindow: "3000"
+    ]
+
+    for {field, bad_value} <- @bad_types do
+      test "invalid #{field} type" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query()
+                 ~>> Map.from_struct()
+                 ~>> Map.put(unquote(field), unquote(Macro.escape(bad_value)))
+                 ~>> then(&struct(OrderAmendments.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+  end
+end
