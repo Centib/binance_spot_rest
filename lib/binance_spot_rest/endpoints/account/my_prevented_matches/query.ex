6dacf2b3e635cc689334a2aa0dee6ad978a9b2a0
Zoran Radovanovic <muvacc@gmail.com>
2025-07-08 15:14:52 +0200

    Add my prevented matches get query

---
+++ lib/binance_spot_rest/endpoints/account/my_prevented_matches/query.ex
@@ -0,0 +1,88 @@
+defmodule BinanceSpotRest.Endpoints.Account.MyPreventedMatches.Query do
+  @moduledoc """
+  ### Query Prevented Matches (USER_DATA)
+
+  ```
+  GET /api/v3/myPreventedMatches
+  ```
+
+  Displays the list of orders that were expired due to STP.
+
+  These are the combinations supported:
+
+  - `symbol` + `preventedMatchId`
+  - `symbol` + `orderId`
+  - `symbol` + `orderId` + `fromPreventedMatchId` (`limit` will default to 500)
+  - `symbol` + `orderId` + `fromPreventedMatchId` + `limit`
+
+  **Parameters:**
+
+  | Name                 | Type   | Mandatory | Description                              |
+  | -------------------- | ------ | --------- | ---------------------------------------- |
+  | symbol               | STRING | YES       |                                          |
+  | preventedMatchId     | LONG   | NO        |                                          |
+  | orderId              | LONG   | NO        |                                          |
+  | fromPreventedMatchId | LONG   | NO        |                                          |
+  | limit                | INT    | NO        | Default: `500`; Maximum: `1000`          |
+  | recvWindow           | LONG   | NO        | The value cannot be greater than `60000` |
+  | timestamp            | LONG   | YES       |                                          |
+
+  **Weight:**
+
+  | Case                           | Weight |
+  | ------------------------------ | ------ |
+  | If `symbol` is invalid         | 2      |
+  | Querying by `preventedMatchId` | 2      |
+  | Querying by `orderId`          | 20     |
+
+  **Data Source:**
+
+  Database
+
+  **Response:**
+
+  ```json
+  [
+    {
+      "symbol": "BTCUSDT",
+      "preventedMatchId": 1,
+      "takerOrderId": 5,
+      "makerSymbol": "BTCUSDT",
+      "makerOrderId": 3,
+      "tradeGroupId": 1,
+      "selfTradePreventionMode": "EXPIRE_MAKER",
+      "price": "1.100000",
+      "makerPreventedQuantity": "1.300000",
+      "transactTime": 1669101687094
+    }
+  ]
+  ```
+  """
+
+  defstruct [:symbol, :preventedMatchId, :orderId, :fromPreventedMatchId, :limit, :recvWindow]
+
+  defimpl BinanceSpotRest.Query do
+    def validate(q),
+      do:
+        q
+        |> Valpa.string(:symbol)
+        |> Valpa.maybe_integer(:preventedMatchId)
+        |> Valpa.maybe_integer(:orderId)
+        |> Valpa.maybe_integer(:fromPreventedMatchId)
+        |> Valpa.map_exclusive_keys([:preventedMatchId, :orderId])
+        |> Valpa.map_exclusive_optional_keys([:preventedMatchId, :fromPreventedMatchId])
+        |> Valpa.map_exclusive_optional_keys([:preventedMatchId, :limit])
+        |> Valpa.maybe_integer_in_range(:limit, %{min: 0, max: 1000})
+        |> Valpa.maybe_integer_in_range(:recvWindow, %{min: 0, max: 60_000})
+
+    def prepare(q),
+      do: %BinanceSpotRest.Query.RequestSpec{
+        metadata: %BinanceSpotRest.Query.EndpointMetadata{
+          endpoint: "/api/v3/myPreventedMatches",
+          method: BinanceSpotRest.Enums.Method._get(),
+          security_type: BinanceSpotRest.Enums.SecurityType._USER_DATA()
+        },
+        query: q
+      }
+  end
+end
---
+++ test/binance_spot_rest/endpoints/account/my_prevented_matches_test.exs
@@ -0,0 +1,210 @@
+defmodule BinanceSpotRest.Endpoints.Account.MyPreventedMatchesTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  import Loe
+
+  alias BinanceSpotRest.Endpoints.Account.MyPreventedMatches
+
+  def full_valid_query_with_prevented_match_id do
+    %MyPreventedMatches.Query{
+      symbol: "LTCBTC",
+      preventedMatchId: 1234,
+      recvWindow: 3000
+    }
+  end
+
+  def full_valid_query_with_order_id do
+    %MyPreventedMatches.Query{
+      symbol: "LTCBTC",
+      orderId: 1234,
+      fromPreventedMatchId: 5678,
+      limit: 600,
+      recvWindow: 3000
+    }
+  end
+
+  describe "request" do
+    test "all params" do
+      assert {:ok, request} =
+               full_valid_query_with_prevented_match_id()
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request(
+                 base_url: "https://fake.binance.url",
+                 headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+                 timestamp_fn: fn -> 1_740_587_673_449 end,
+                 signature_fn: fn _, _ -> "fake_signature" end
+               )
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: BinanceSpotRest.Enums.Method._get(),
+               headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+               base_url: "https://fake.binance.url",
+               url:
+                 "/api/v3/myPreventedMatches?" <>
+                   "preventedMatchId=1234&" <>
+                   "recvWindow=3000&" <>
+                   "symbol=LTCBTC&" <>
+                   "timestamp=1740587673449&" <>
+                   "signature=fake_signature"
+             }
+
+      assert {:ok, request} =
+               full_valid_query_with_order_id()
+               ~>> BinanceSpotRest.Query.validate()
+               ~>> BinanceSpotRest.Query.prepare()
+               ~>> BinanceSpotRest.Client.create_request(
+                 base_url: "https://fake.binance.url",
+                 headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+                 timestamp_fn: fn -> 1_740_587_673_449 end,
+                 signature_fn: fn _, _ -> "fake_signature" end
+               )
+
+      assert request == %BinanceSpotRest.Client.Request{
+               method: BinanceSpotRest.Enums.Method._get(),
+               headers: [{"FAKE_API_KEY", "fake1234api5678key"}],
+               base_url: "https://fake.binance.url",
+               url:
+                 "/api/v3/myPreventedMatches?" <>
+                   "fromPreventedMatchId=5678&" <>
+                   "limit=600&" <>
+                   "orderId=1234&" <>
+                   "recvWindow=3000&" <>
+                   "symbol=LTCBTC&" <>
+                   "timestamp=1740587673449&" <>
+                   "signature=fake_signature"
+             }
+    end
+  end
+
+  describe "validation (required):" do
+    @required [:symbol]
+
+    for field <- @required do
+      test "invalid without #{field}" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query_with_prevented_match_id()
+                 ~>> Map.from_struct()
+                 ~>> Map.delete(unquote(field))
+                 ~>> then(&struct(MyPreventedMatches.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query_with_order_id()
+                 ~>> Map.from_struct()
+                 ~>> Map.delete(unquote(field))
+                 ~>> then(&struct(MyPreventedMatches.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+  end
+
+  describe "validation (optional):" do
+    @optional_with_prevented_match_id [:recvWindow]
+
+    @optional_with_order_id [:fromPreventedMatchId, :limit, :recvWindow]
+
+    test "valid without optional fields" do
+      assert {:ok, %MyPreventedMatches.Query{}} =
+               full_valid_query_with_prevented_match_id()
+               ~>> Map.from_struct()
+               ~>> Map.drop(@optional_with_prevented_match_id)
+               ~>> then(&struct(MyPreventedMatches.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+
+      assert {:ok, %MyPreventedMatches.Query{}} =
+               full_valid_query_with_order_id()
+               ~>> Map.from_struct()
+               ~>> Map.drop(@optional_with_order_id)
+               ~>> then(&struct(MyPreventedMatches.Query, &1))
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+
+  describe "validation (type):" do
+    @bad_types_with_prevented_match_id [
+      symbol: :LTCBTC,
+      preventedMatchId: "1234",
+      recvWindow: "3000"
+    ]
+
+    @bad_types_with_order_id [
+      orderId: "1234",
+      fromPreventedMatchId: "5678",
+      limit: "600"
+    ]
+
+    for {field, bad_value} <- @bad_types_with_prevented_match_id do
+      test "invalid #{field} type" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query_with_prevented_match_id()
+                 ~>> Map.from_struct()
+                 ~>> Map.put(unquote(field), unquote(Macro.escape(bad_value)))
+                 ~>> then(&struct(MyPreventedMatches.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+
+    for {field, bad_value} <- @bad_types_with_order_id do
+      test "invalid #{field} type" do
+        assert {:error, %{field: unquote(field)}} =
+                 full_valid_query_with_order_id()
+                 ~>> Map.from_struct()
+                 ~>> Map.put(unquote(field), unquote(Macro.escape(bad_value)))
+                 ~>> then(&struct(MyPreventedMatches.Query, &1))
+                 ~>> BinanceSpotRest.Query.validate()
+      end
+    end
+  end
+
+  describe "validation (exclusive keys):" do
+    test "invalid if preventedMatchId is with orderId" do
+      assert {:error, %{validator: :map_exclusive_keys}} =
+               %MyPreventedMatches.Query{
+                 symbol: "LTCBTC",
+                 preventedMatchId: 1234,
+                 orderId: 5678,
+                 recvWindow: 3000
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+
+  describe "validation (exclusive optional keys):" do
+    test "invalid if preventedMatchId is with fromPreventedMatchId and limit" do
+      assert {:error, %{validator: :map_exclusive_optional_keys}} =
+               %MyPreventedMatches.Query{
+                 symbol: "LTCBTC",
+                 preventedMatchId: 1234,
+                 fromPreventedMatchId: 5678,
+                 limit: 600,
+                 recvWindow: 3000
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "invalid if preventedMatchId is with fromPreventedMatchId" do
+      assert {:error, %{validator: :map_exclusive_optional_keys}} =
+               %MyPreventedMatches.Query{
+                 symbol: "LTCBTC",
+                 preventedMatchId: 1234,
+                 fromPreventedMatchId: 5678,
+                 recvWindow: 3000
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+
+    test "invalid if preventedMatchId is with limit" do
+      assert {:error, %{validator: :map_exclusive_optional_keys}} =
+               %MyPreventedMatches.Query{
+                 symbol: "LTCBTC",
+                 preventedMatchId: 1234,
+                 limit: 600,
+                 recvWindow: 3000
+               }
+               ~>> BinanceSpotRest.Query.validate()
+    end
+  end
+end
