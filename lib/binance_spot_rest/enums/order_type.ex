bd6233f256328620235094b3b5293c6fb51e507d
Zoran Radovanovic <muvacc@gmail.com>
2025-07-11 14:52:21 +0200

    Remove unused enums

--- lib/binance_spot_rest/enums/order_type.ex
+++ lib/binance_spot_rest/enums/order_type.ex
@@ -9,15 +9,4 @@ defmodule BinanceSpotRest.Enums.OrderType do
     :TAKE_PROFIT_LIMIT,
     :LIMIT_MAKER
   ]
-
-  defmodule Oco do
-    @moduledoc false
-    use Enuma, [
-      :STOP_LOSS,
-      :STOP_LOSS_LIMIT,
-      :TAKE_PROFIT,
-      :TAKE_PROFIT_LIMIT,
-      :LIMIT_MAKER
-    ]
-  end
 end
